f1382ab5526828cefc7d50476be06d35dd508426
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2025-02-03 15:08:38 -0500

    Initial Commit

---
+++ Grocery Flyers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grocery Flyers
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-02-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grocery Flyers/Grocery_FlyersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_FlyersApp.swift
+//  Grocery Flyers
+//
+//  Created by Skye Willow Harris-Stoertz on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_FlyersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
